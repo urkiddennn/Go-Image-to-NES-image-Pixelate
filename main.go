7c3903036921a53c06284d24fdf309a883654e16
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2025-05-23 15:33:41 +0800

    Update: fully functional Go changing color to NEX palette

--- main.go
+++ main.go
@@ -4,47 +4,91 @@ import (
 	"fmt"
 	"image"
 	"image/color"
-	"image/png"
+	"image/jpeg"
+	"math"
 	"os"
 )
 
-func main() {
-	// Define a Custom NES-like color palette
-	palette := color.Palette{
-		color.RGBA{0x7C, 0x7C, 0x7C, 0xFF}, // Light gray
-		color.RGBA{0x00, 0x00, 0xFC, 0xFF}, // Blue
-		color.RGBA{0x00, 0x00, 0xBC, 0xFF}, // Dark blue
-		color.RGBA{0x44, 0x28, 0xBC, 0xFF}, // Purple
-		color.RGBA{0x94, 0x00, 0x84, 0xFF}, // Magenta
-		color.RGBA{0xA8, 0x00, 0x20, 0xFF}, // Red
-		color.RGBA{0xA8, 0x10, 0x00, 0xFF}, // Dark red
-		color.RGBA{0x88, 0x14, 0x00, 0xFF}, // Brown
-		color.RGBA{0x50, 0x30, 0x00, 0xFF}, // Dark brown
-		color.RGBA{0x00, 0x78, 0x00, 0xFF}, // Green
-		color.RGBA{0x00, 0x68, 0x00, 0xFF}, // Dark green
-		color.RGBA{0x00, 0x58, 0x00, 0xFF}, // Olive
-		color.RGBA{0x00, 0x40, 0x58, 0xFF}, // Teal
-		color.RGBA{0x00, 0x00, 0x00, 0xFF},
-	}
-	img := image.NewPaletted(image.Rect(0, 0, 100, 100), palette)
+var NesPalette = []color.RGBA{
+	{0x7C, 0x7C, 0x7C, 0xFF},
+	{0x00, 0x00, 0xFC, 0xFF},
+	{0x00, 0x00, 0xBC, 0xFF},
+	{0x44, 0x28, 0xBC, 0xFF},
+	{0x94, 0x00, 0x84, 0xFF},
+	{0xA8, 0x00, 0x20, 0xFF},
+	{0xA8, 0x10, 0x00, 0xFF},
+	{0x88, 0x14, 0x00, 0xFF},
+	{0x50, 0x30, 0x00, 0xFF},
+	{0x00, 0x78, 0x00, 0xFF},
+	{0x00, 0x68, 0x00, 0xFF},
+	{0x00, 0x58, 0x00, 0xFF},
+	{0x00, 0x40, 0x58, 0xFF},
+	{0x00, 0x00, 0x00, 0xFF},
+	{0x00, 0x00, 0x00, 0xFF},
+	{0x00, 0x00, 0x00, 0xFF},
+}
 
-	for y := 0; y < 100; y++ {
-		for x := 0; x < 100; x++ {
-			colorIndex := uint8((x + y) % len(palette))
-			img.SetColorIndex(x, y, colorIndex)
+// Find the colosest color to the NES palette
+func closestColor(c color.Color) color.Color {
+	r, g, b, _ := c.RGBA()
+	minDistance := math.MaxFloat64
+	var closest color.RGBA
+
+	for _, paletteColor := range NesPalette {
+		pr, pg, pb := float64(paletteColor.R), float64(paletteColor.G), float64(paletteColor.B)
+
+		distance := math.Sqrt(math.Pow(float64(r>>8)-pr, 2) + math.Pow(float64(g>>8)-pg, 2) + math.Pow(float64(b>>8)-pb, 2))
+		if distance < minDistance {
+			minDistance = distance
+			closest = paletteColor
 		}
 	}
-	// handle Errors
-	file, err := os.Create("output.png")
+
+	return closest
+}
+
+// Apply the NesPalette
+func applyNESColorPalette(img image.Image) image.Image {
+	bounds := img.Bounds()
+	newImg := image.NewRGBA(bounds)
+
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			originalColor := img.At(x, y)
+			newImg.Set(x, y, closestColor(originalColor))
+		}
+	}
+	return newImg
+}
+
+func main() {
+	// Define a Custom NES-like color palette
+	// get the image file
+	imgFile, err := os.Open("reck.jpg")
 	if err != nil {
 		panic(err)
 	}
-	defer file.Close()
-
-	err = png.Encode(file, img)
+	defer imgFile.Close()
+	// Decode the image file
+	img, _, err := image.Decode(imgFile)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Created the output")
+	nesImg := applyNESColorPalette(img)
+
+	ouputFile, err := os.Create("output2.jpg")
+	if err != nil {
+		panic(err)
+	}
+	defer ouputFile.Close()
+
+	jpegOptions := &jpeg.Options{Quality: 2}
+
+	err = jpeg.Encode(ouputFile, nesImg, jpegOptions)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Image coverted to NES palette Successfully!")
 }
